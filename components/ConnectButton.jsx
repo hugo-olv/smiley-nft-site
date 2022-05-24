635397f14780f7ffa66328f132261d4c3bf57234
themaskedcat <oliveira.developpement@gmail.com>
2022-05-24 22:22:38 +0200

    Move MetamaskContext direclty into ConnectButton component

--- components/ConnectButton.jsx
+++ components/ConnectButton.jsx
@@ -1,15 +1,28 @@
-export const ConnectButton = ({ shortAddress, isConnected = false, onClick }) => {
+import { useContext } from 'react'
+import { MetamaskContext } from '../context'
+import { shortenAddress } from '../utils/shortenAddress'
+
+export const ConnectButton = () => {
+    const {
+        metaState: { accounts, isConnected },
+        loading,
+        connect,
+    } = useContext(MetamaskContext)
+
     const styles = `
     ${!isConnected && 'hover:bg-[#763E1A]'}
     bg-[#F4841F] font-medium text-white py-2 px-4 rounded
     `
+
+    const text = shortenAddress(accounts[0]) || 'Connect Wallet'
+
     return (
         <button
             className={styles}
             disabled={isConnected}
-            onClick={onClick}
+            onClick={connect}
         >
-            {shortAddress || 'Connect Wallet'}
+            {text}
         </button>
     )
 }
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,14 +1,6 @@
 import { ConnectButton } from './ConnectButton'
-import { useContext } from 'react'
-import { MetamaskContext } from '../context'
-import { shortenAddress } from '../utils/shortenAddress'
 
 export const Layout = ({ children }) => {
-    const {
-        metaState: { accounts, chain, balance, isConnected },
-        loading,
-        connect,
-    } = useContext(MetamaskContext)
 
     return (
         <div className='flex flex-col h-screen bg-gradient-to-tr from-indigo-500 to-blue-500 text-white'>
@@ -16,11 +8,7 @@ export const Layout = ({ children }) => {
             <div className='flex justify-between px-8 py-3'>
                 <div></div>
                 <div>
-                    <ConnectButton
-                        shortAddress={shortenAddress(accounts[0])}
-                        isConnected={isConnected}
-                        onClick={connect}
-                    />
+                    <ConnectButton />
                 </div>
             </div>
             <div className='flex flex-1 justify-center align-middle'>
